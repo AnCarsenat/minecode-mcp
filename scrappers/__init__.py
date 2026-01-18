bc6ff25b351a5c5a048bc397be723b782c24b3c0
ancarsenart <antoine.carsenat@gmail.com>
2026-01-18 12:37:00 +0100

    Clean up: remove duplicate files, keep only minecode package

--- scrappers/__init__.py
+++
@@ -1,7 +0,0 @@
-# MineCode Scrappers Module
-from . import mojira
-from . import minecraftwiki
-from . import spyglass
-from . import misode
-
-__all__ = ["mojira", "minecraftwiki", "spyglass", "misode"]
